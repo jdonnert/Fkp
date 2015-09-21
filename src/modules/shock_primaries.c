a504e8ca8a2f3e0dee0c4f694a1155ce21b33bd6
Julius Donnert <donnert@ira.inaf.it>
2015-09-21 17:04:38 -0500

    fix shock injection. missed mec in pinj

--- src/modules/shock_primaries.c
+++ src/modules/shock_primaries.c
@@ -9,13 +9,13 @@
 
 #include "modules.h"
 
-#define BISECTION_ACCURACY 1e-3
+#define BISECTION_ACCURACY 1e-4
 #define NSTEP 2048 // err < 5e-3 in norm integral
 
 #define HIGHCUT 1e3     // Upper Energy Cut of PowerLaw 
 #define LOWCUT 50       // Lower Energy Cut of PowerLaw 
 #define INDEX -2.0		// Spectral index of power-law
-#define X_CRE 0.001
+#define X_CRE 0.01
 
 static double cre_spectrum_hadronic_background(const double, const double);
 static double maxwellian(const double T, const double p, const double nth);
@@ -30,9 +30,8 @@ static void downstream_quantities(const struct Gas_Data, double *Temp_dw,
 static double Integral = 0;
 
 void Injection(const struct Gas_Data SphP_i, const double *p_cgs, 
-		const int nGrid, const double dt, double *Q)
+		const int nGrid, const double dt, double * Q)
 {
-
 #ifdef Q_SHOCK_WITH_HADRONIC_BACKGROUND
     double eps_therm = thermal_energy_density_cgs(SphP_i.U, SphP_i.Rho);
 	
@@ -49,54 +48,11 @@ void Injection(const struct Gas_Data SphP_i, const double *p_cgs,
 
 	double CRe_norm = cre_spectrum_normalisation(SphP_i, s);
 
-	//double dt_particle = SphP_i.Hsml / vshock * Unit.Time;
-	//double dt_snap = (Snap.Time - Snap.LastTime) * Unit.Time;
-	
 	for (int i = 0; i < nGrid; i++) 
 		Q[i] = cre_spectrum(CRe_norm, p_cgs[i], s) / dt; 
 
 	return ;
 }
-
-
-#ifdef Q_SHOCK_WITH_HADRONIC_BACKGROUND
-
-static double cre_spectrum_hadronic_background(const double norm, 
-		const double p)
-{
-	const double gamma = p/(m_e*c);
-
-    if (gamma < LOWCUT || gamma > HIGHCUT)
-	    return 0;
-
-	return norm *  pow(gamma , INDEX) * sqrt(1 - (gamma) /HIGHCUT) 
-	    * (1 - LOWCUT/(gamma));
-}
-
-void Init_Hadronic_Background()
-{
-	const double H = HIGHCUT, L = LOWCUT;
-
-    double x = H;
-
-    Integral = -1.0/(8*p2(x)*p2(H)) * (p2(x)*(L-4*H)*log(1-sqrt(1-x/H)) 
-		+ p2(x) * (4*H-L)*log(sqrt(1-x/H)+1)
-		- 2*H*sqrt(1-x/H) * (-2*H*L + 4*H*x + L*x));
-
-    x = L;
-
-    Integral +=  -1.0/(8*p2(x)*p2(H)) * (p2(x)*(L-4*H)*log(1-sqrt(1-x/H)) 
-		+ p2(x) * (4*H-L)*log(sqrt(1-x/H)+1)
-		- 2*H*sqrt(1-x/H) * (-2*H*L + 4*H*x + L*x));
-	
-	printf("Shock Primaries & Hadronic Background Q(p):"
-			" Low=%g, High=%g, Norm=%g \n", L, H, 1/Integral);
-
-    return;
-	
-}
-#endif
-
 
 static double cre_spectrum(const double norm, const double p, const double s)
 {
@@ -117,7 +73,7 @@ static double cre_spectrum_normalisation(const struct Gas_Data SphP_i,
 
 	double cre_norm = 0;
 
-	double left = sqrt(2*m_e*k_B*temp_dw) /(m_e*c); // peak of maxwellian
+	double left = sqrt(2*m_e*k_B*temp_dw); // peak of maxwellian
 	double right = left * 30;	// maxwellian is very steep
 
 	double delta = 1, 
@@ -129,16 +85,16 @@ static double cre_spectrum_normalisation(const struct Gas_Data SphP_i,
 
 		p_inj = left + 0.5 * (right-left);
 
-		double n_maxw = maxwellian(temp_dw, p_inj*m_e*c, nth_dw);
+		double n_maxw = maxwellian(temp_dw, p_inj, nth_dw);
 
-		cre_norm = cre_norm_fac / cre_energy_integral(s, p_inj);
+		cre_norm = cre_norm_fac / cre_energy_integral(s, p_inj/(m_e*c));
 
-		double n_cre = cre_spectrum(cre_norm, p_inj*m_e*c, s);
+		double n_cre = cre_spectrum(cre_norm, p_inj, s);
 
 		delta = (n_maxw - n_cre) / (n_maxw + n_cre); // weighted difference
 
 #ifdef DEBUG
-if (SphP_i.ID == 3866445) 
+	if (SphP_i.ID == 1607043) 
 		printf("%g %g %g %g %g %g %g %g %d\n", left, p_inj, right, n_maxw, 
 				n_cre, delta, cre_norm, cre_energy_integral(s, p_inj), it++);
 #endif
@@ -150,7 +106,8 @@ if (SphP_i.ID == 3866445)
 	}
 
 #ifdef DEBUG
-	printf("Found p_inj=%g CRe_norm=%g \n ", p_inj, cre_norm);
+	if (SphP_i.ID == 1607043) 
+		printf("Found M=%g p_inj=%g CRe_norm=%g \n ",SphP_i.Mach, p_inj/m_e/c, cre_norm);
 #endif
 
 	return cre_norm;
@@ -195,6 +152,12 @@ static void downstream_quantities(const struct Gas_Data SphP_i, double *Temp_dw,
 
 	*EpsCRe = eta_cre * (epsTherm_dw - epsTherm_up * pow(sigma, gamma)); 
 		// cgs, Ensslin+ 2007
+
+if (SphP_i.ID == 1607043) 
+	printf("EpsCRe = %g, epsdw=%g epsup=%g sigma=%g mach=%g "
+			"rho_up=%g pres_up=%g ID=%d \n", 
+			*EpsCRe, epsTherm_dw,epsTherm_up, sigma, mach, rho_up,
+			pres_up, SphP_i.ID);
 
 	if (*EpsCRe < 0 || !isfinite(*EpsCRe)) {
 
@@ -281,6 +244,45 @@ static double cre_spectral_index(double mach)
 
 	return s;
 }
+
+#ifdef Q_SHOCK_WITH_HADRONIC_BACKGROUND
+
+static double cre_spectrum_hadronic_background(const double norm, 
+		const double p)
+{
+	const double gamma = p/(m_e*c);
+
+    if (gamma < LOWCUT || gamma > HIGHCUT)
+	    return 0;
+
+	return norm *  pow(gamma , INDEX) * sqrt(1 - (gamma) /HIGHCUT) 
+	    * (1 - LOWCUT/(gamma));
+}
+
+void Init_Hadronic_Background()
+{
+	const double H = HIGHCUT, L = LOWCUT;
+
+    double x = H;
+
+    Integral = -1.0/(8*p2(x)*p2(H)) * (p2(x)*(L-4*H)*log(1-sqrt(1-x/H)) 
+		+ p2(x) * (4*H-L)*log(sqrt(1-x/H)+1)
+		- 2*H*sqrt(1-x/H) * (-2*H*L + 4*H*x + L*x));
+
+    x = L;
+
+    Integral +=  -1.0/(8*p2(x)*p2(H)) * (p2(x)*(L-4*H)*log(1-sqrt(1-x/H)) 
+		+ p2(x) * (4*H-L)*log(sqrt(1-x/H)+1)
+		- 2*H*sqrt(1-x/H) * (-2*H*L + 4*H*x + L*x));
+	
+	printf("Shock Primaries & Hadronic Background Q(p):"
+			" Low=%g, High=%g, Norm=%g \n", L, H, 1/Integral);
+
+    return;
+	
+}
+#endif
+
 
 #undef NSTEP
 #undef BISECTION_ACCURACY 
